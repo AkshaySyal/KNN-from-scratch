e417002152113c8b22ac74a2220eb0e392369069
Akshay Syal <akshaysyal@Akshays-Laptop.local>
2024-05-28 17:31:43 -0400

    almost done

--- train.py
+++ train.py
@@ -1,14 +1,23 @@
 import numpy as np
 import idx2numpy
 import matplotlib.pyplot as plt
+from KNN import KNN
+from importlib import reload
 
 
-
+reload(KNN)
 train_images = idx2numpy.convert_from_file("dataset/train-images.idx3-ubyte")
+train_images_copy = np.copy(train_images)
 train_labels = idx2numpy.convert_from_file("dataset/train-labels.idx1-ubyte")
+
 test_images = idx2numpy.convert_from_file("dataset/t10k-images.idx3-ubyte")
+test_images_copy = np.copy(test_images)
 test_labels = idx2numpy.convert_from_file("dataset/t10k-labels.idx1-ubyte")
 
+clf = KNN(k=5)
 
-print(train_images.shape)
-print(test_images.shape)
+clf.fit(train_images_copy,train_labels)
+
+train_images_copy[0]
+
+clf.predict_single(test_images_copy[0])
