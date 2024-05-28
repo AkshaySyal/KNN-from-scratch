1c75471bf3877d9abfb787c683027969d93e3a3b
Akshay Syal <akshaysyal@Akshays-Laptop.local>
2024-05-28 15:53:37 -0400

    transform and fit

--- KNN.py
+++ KNN.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+class KNN:
+    def __init__(self,k):
+        self.k = k
+    
+    def fit(self,data,labels):
+        self.data = data
+        self.labels = labels
+    
+    
