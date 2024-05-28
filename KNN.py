7eaa6a2d5fcfdf31333fc5a9fd72b45d19a5ec4b
Akshay Syal <akshaysyal@Akshays-Laptop.local>
2024-05-28 16:12:29 -0400

    fit and transform

--- KNN.py
+++ KNN.py
@@ -4,8 +4,11 @@ class KNN:
     def __init__(self,k):
         self.k = k
     
+    def transform(self,data):
+        data = data.reshape(data.shape[0],-1) # Flattening images, (60k,28,28) -> (60k,784)
+        data[data > 0] = 1 # Boolean Indexing
+        return data
+
     def fit(self,data,labels):
         self.data = data
         self.labels = labels
-    
-    
